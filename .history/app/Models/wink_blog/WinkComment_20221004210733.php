e248ee03c1488dab819ec920ebb43c9841837e9b
Mathew Juma <athias85@gmail.com>
2022-10-04 21:13:45 +0300

    Changes to WinkPost

---
+++ .history/app/Models/wink_blog/WinkComment_20221004210733.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\wink_blog;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WinkComment extends Model
+{
+    use HasFactory;
+}
---
+++ .history/app/Models/wink_blog/WinkComment_20221004210839.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models\wink_blog;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WinkComment extends Model
+{
+    use HasFactory;
+
+     /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'wink_comments';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that should be casted.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'meta' => 'array',
+    ];
+
+    /**
+     * The posts that has the tag.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function posts()
+    {
+        return $this->belongsTo(WinkPost::class, 'post_id');
+    }
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($item) {
+            $item->posts()->detach();
+        });
+    }
+}
